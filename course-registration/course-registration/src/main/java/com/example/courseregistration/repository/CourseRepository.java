b95631b4377ce8141348d6b6219a30aa0d7067b1
Maheswarareddy Pulagam <pulagammaheswarareddy2005@gmail.com>
2026-08-07 14:39:33 +0530

    Delete course-registration directory

--- course-registration/course-registration/src/main/java/com/example/courseregistration/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.courseregistration.repository;
-
-import com.example.courseregistration.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
--- course-registration/course-registration/src/main/java/com/example/courseregistration/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.courseregistration.repository;
-
-import com.example.courseregistration.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
